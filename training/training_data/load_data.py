4db9add8501036164193194dfc76844a3c9063c5
faiza-alvi <bookisharchitect@gmail.com>
2025-07-17 11:45:52 -0700

    Adjust row iteration for derivative calc

--- training/training_data/load_data.py
+++ training/training_data/load_data.py
@@ -264,7 +264,7 @@ df = df.sample(fraction=1, with_replacement=False, shuffle=True, seed=0)
 
 # Make the derivative dataset
 # Apply to all rows
-derivatives_df = pd.DataFrame([compute_derivatives(row) for _, row in df.iter_rows(named=True)])
+derivatives_df = pl.DataFrame([compute_derivatives(row) for row in df.iter_rows(named=True)])
 
 # Make the scaled datasets
 df_inputs_scaled = pl.DataFrame(
