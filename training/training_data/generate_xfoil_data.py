033bc9dc92854bdcd3ca21d9c2a566159a1d8f74
faiza-alvi~ <bookisharchitect@gmail.com~>
2025-08-18 13:16:27 -0700

    Change file name for last generated data

--- training/training_data/generate_xfoil_data.py
+++ training/training_data/generate_xfoil_data.py
@@ -14,7 +14,7 @@ ray.init(
     # num_cpus=2,
 )
 
-datafile = "data_xfoil_final_K3.csv"
+datafile = "data_xfoil_final_K4.csv"
 n_procs = int(ray.cluster_resources()["CPU"])
 print(f"Running on {n_procs} processes.")
 
