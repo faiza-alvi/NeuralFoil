1baed43650d1564137bd4aaa2d22fdd2e64a3e9a
faiza-alvi~ <bookisharchitect@gmail.com~>
2025-11-24 14:15:07 -0800

    Change to version that plots generated data

--- training/training_data/visualize_data.py
+++ training/training_data/visualize_data.py
@@ -15,186 +15,191 @@ cols = Data.get_vector_column_names()
 ### Read the original data, by scraping all .csv files within the data directory
 # data_directory = Path(r"/home/faiza/Documents/NeuralFoil/training/training_data")
 # data_directory = Path(r"/home/faiza/Downloads/training_data/training_data")
-airfoil_database_path = Path("/home/faiza/Documents/TrainingAirfoils")
 
-def load_airfoil_coordinates(filepath):
-    # Skip the first line (airfoil name), and load the x,y columns
-    with open(filepath, 'r') as f:
-        lines = f.readlines()
+# airfoil_database_path = Path("/home/faiza/Documents/TrainingAirfoils")
+data_directory = Path("/home/faiza/Documents/NeuralFoil")
 
-    # Remove any empty lines and non-numeric lines
-    coords = []
-    for line in lines[1:]:  # skip the first line (usually name/title)
-        try:
-            x, y = map(float, line.strip().split())
-            coords.append([x, y])
-        except ValueError:
-            continue  # Skip lines that can't be parsed
+# ________________________________________
+# USE BELOW WHEN CYCLING THROUGH .DAT FILES OF AIRFOILS 
 
-    return np.array(coords)
+# def load_airfoil_coordinates(filepath):
+#     # Skip the first line (airfoil name), and load the x,y columns
+#     with open(filepath, 'r') as f:
+#         lines = f.readlines()
 
-airfoil_database = [
-    asb.Airfoil(
-        name=filename.stem,
-        coordinates=load_airfoil_coordinates(filename)
-    ).normalize().to_kulfan_airfoil()
-    for filename in airfoil_database_path.glob("*.dat")
-]
+#     # Remove any empty lines and non-numeric lines
+#     coords = []
+#     for line in lines[1:]:  # skip the first line (usually name/title)
+#         try:
+#             x, y = map(float, line.strip().split())
+#             coords.append([x, y])
+#         except ValueError:
+#             continue  # Skip lines that can't be parsed
 
-### Compute the covariance matrix of airfoil shape parameters, for better data generation later
-kulfans_database = np.stack(
-    [
-        np.concatenate(
-            [
-                airfoil.upper_weights,
-                airfoil.lower_weights,
-                np.atleast_1d(airfoil.leading_edge_weight),
-                np.atleast_1d(airfoil.TE_thickness),
-            ]
-        )
-        for airfoil in airfoil_database
-    ],
-    axis=0,
-)
-mean_database = np.mean(kulfans_database, axis=0)
-cov_database = np.cov(kulfans_database, rowvar=False)
+#     return np.array(coords)
 
-print("mean and cov of kulfans database")
-print(mean_database)
-print(cov_database)
+# airfoil_database = [
+#     asb.Airfoil(
+#         name=filename.stem,
+#         coordinates=load_airfoil_coordinates(filename)
+#     ).normalize().to_kulfan_airfoil()
+#     for filename in airfoil_database_path.glob("*.dat")
+# ]
 
-
-# raw_dfs = {}
-
-# for csv_file in data_directory.glob("data*.csv"):
-#     print(f"Reading {csv_file}...")
-#     raw_dfs[csv_file.stem] = pl.read_csv(
-#         csv_file, has_header=False, new_columns=cols, schema_overrides={col: pl.Float32 for col in cols}, infer_schema_length=10000, skip_rows=1
-#     )
-#     print(f"\t{len(raw_dfs[csv_file.stem])} rows")
-
-# df = pl.concat(raw_dfs.values())
-
-# # Do some basic cleanup
-# cols_to_nullify = Data.get_vector_output_column_names().copy()
-# cols_to_nullify.remove("analysis_confidence")
-
-# c = pl.col("CD") <= 0
-# print(f"Nullifying {int(df.select(c).sum().to_numpy()[0, 0])} rows with CD <= 0...")
-# df = df.with_columns(
+# ### Compute the covariance matrix of airfoil shape parameters, for better data generation later
+# kulfans_database = np.stack(
 #     [
-#         pl.when(c)
-#         .then(0)
-#         .otherwise(pl.col("analysis_confidence"))
-#         .alias("analysis_confidence"),
-#     ]
-#     + [
-#         pl.when(c).then(None).otherwise(pl.col(col)).alias(col)
-#         for col in cols_to_nullify
-#     ]
-# )
-
-# c = pl.any_horizontal(
-#     [pl.col(f"upper_bl_theta_{i}") <= 0 for i in range(Data.N)]
-#     + [pl.col(f"lower_bl_theta_{i}") <= 0 for i in range(Data.N)]
-# )
-# print(
-#     f"Nullifying {int(df.select(c).sum().to_numpy()[0, 0])} rows with nonpositive boundary layer thetas..."
-# )
-# df = df.with_columns(
-#     [
-#         pl.when(c)
-#         .then(0)
-#         .otherwise(pl.col("analysis_confidence"))
-#         .alias("analysis_confidence"),
-#     ]
-#     + [
-#         pl.when(c).then(None).otherwise(pl.col(col)).alias(col)
-#         for col in cols_to_nullify
-#     ]
-# )
-
-# c = pl.any_horizontal(
-#     [pl.col(f"upper_bl_H_{i}") < 1 for i in range(Data.N)]
-#     + [pl.col(f"lower_bl_H_{i}") < 1 for i in range(Data.N)]
-# )
-# print(
-#     f"Nullifying {int(df.select(c).sum().to_numpy()[0, 0])} rows with H < 1 (non-physical BL)..."
-# )
-# df = df.with_columns(
-#     [
-#         pl.when(c)
-#         .then(0)
-#         .otherwise(pl.col("analysis_confidence"))
-#         .alias("analysis_confidence"),
-#     ]
-#     + [
-#         pl.when(c).then(None).otherwise(pl.col(col)).alias(col)
-#         for col in cols_to_nullify
-#     ]
-# )
-
-# c = pl.any_horizontal(
-#     sum(
-#         [
+#         np.concatenate(
 #             [
-#                 pl.col(f"upper_bl_ue/vinf_{i}") < -20,
-#                 pl.col(f"upper_bl_ue/vinf_{i}") > 20,
-#                 pl.col(f"lower_bl_ue/vinf_{i}") < -20,
-#                 pl.col(f"lower_bl_ue/vinf_{i}") > 20,
+#                 airfoil.upper_weights,
+#                 airfoil.lower_weights,
+#                 np.atleast_1d(airfoil.leading_edge_weight),
+#                 np.atleast_1d(airfoil.TE_thickness),
 #             ]
-#             for i in range(Data.N)
-#         ],
-#         start=[],
-#     )
+#         )
+#         for airfoil in airfoil_database
+#     ],
+#     axis=0,
 # )
-# print(
-#     f"Nullifying {int(df.select(c).sum().to_numpy()[0, 0])} rows with non-physical edge velocities..."
-# )
-# df = df.with_columns(
-#     [
-#         pl.when(c)
-#         .then(0)
-#         .otherwise(pl.col("analysis_confidence"))
-#         .alias("analysis_confidence"),
-#     ]
-#     + [
-#         pl.when(c).then(None).otherwise(pl.col(col)).alias(col)
-#         for col in cols_to_nullify
-#     ]
-# )
+# mean_database = np.mean(kulfans_database, axis=0)
+# cov_database = np.cov(kulfans_database, rowvar=False)
 
-# c = pl.any_horizontal(
-#     pl.col("Top_Xtr") < 0,
-#     pl.col("Top_Xtr") > 1,
-#     pl.col("Bot_Xtr") < 0,
-#     pl.col("Bot_Xtr") > 1,
-# )
-# print(
-#     f"Nullifying {int(df.select(c).sum().to_numpy()[0, 0])} rows with non-physical transition locations..."
-# )
-# df = df.with_columns(
-#     [
-#         pl.when(c)
-#         .then(0)
-#         .otherwise(pl.col("analysis_confidence"))
-#         .alias("analysis_confidence"),
-#     ]
-#     + [
-#         pl.when(c).then(None).otherwise(pl.col(col)).alias(col)
-#         for col in cols_to_nullify
-#     ]
-# # )
+# print("mean and cov of kulfans database")
+# print(mean_database)
+# print(cov_database)
 
-# # Convert just the first 18 columns to a numpy array (float)
-# params_all = df.select(df.columns[:18]).to_numpy()
+# USE BELOW WHEN WORKING WITH .CSV 
+raw_dfs = {}
+
+for csv_file in data_directory.glob("data*.csv"):
+    print(f"Reading {csv_file}...")
+    raw_dfs[csv_file.stem] = pl.read_csv(
+        csv_file, has_header=False, new_columns=cols, schema_overrides={col: pl.Float32 for col in cols}, infer_schema_length=10000, skip_rows=1
+    )
+    print(f"\t{len(raw_dfs[csv_file.stem])} rows")
+
+df = pl.concat(raw_dfs.values())
+
+# Do some basic cleanup
+cols_to_nullify = Data.get_vector_output_column_names().copy()
+cols_to_nullify.remove("analysis_confidence")
+
+c = pl.col("CD") <= 0
+print(f"Nullifying {int(df.select(c).sum().to_numpy()[0, 0])} rows with CD <= 0...")
+df = df.with_columns(
+    [
+        pl.when(c)
+        .then(0)
+        .otherwise(pl.col("analysis_confidence"))
+        .alias("analysis_confidence"),
+    ]
+    + [
+        pl.when(c).then(None).otherwise(pl.col(col)).alias(col)
+        for col in cols_to_nullify
+    ]
+)
+
+c = pl.any_horizontal(
+    [pl.col(f"upper_bl_theta_{i}") <= 0 for i in range(Data.N)]
+    + [pl.col(f"lower_bl_theta_{i}") <= 0 for i in range(Data.N)]
+)
+print(
+    f"Nullifying {int(df.select(c).sum().to_numpy()[0, 0])} rows with nonpositive boundary layer thetas..."
+)
+df = df.with_columns(
+    [
+        pl.when(c)
+        .then(0)
+        .otherwise(pl.col("analysis_confidence"))
+        .alias("analysis_confidence"),
+    ]
+    + [
+        pl.when(c).then(None).otherwise(pl.col(col)).alias(col)
+        for col in cols_to_nullify
+    ]
+)
+
+c = pl.any_horizontal(
+    [pl.col(f"upper_bl_H_{i}") < 1 for i in range(Data.N)]
+    + [pl.col(f"lower_bl_H_{i}") < 1 for i in range(Data.N)]
+)
+print(
+    f"Nullifying {int(df.select(c).sum().to_numpy()[0, 0])} rows with H < 1 (non-physical BL)..."
+)
+df = df.with_columns(
+    [
+        pl.when(c)
+        .then(0)
+        .otherwise(pl.col("analysis_confidence"))
+        .alias("analysis_confidence"),
+    ]
+    + [
+        pl.when(c).then(None).otherwise(pl.col(col)).alias(col)
+        for col in cols_to_nullify
+    ]
+)
+
+c = pl.any_horizontal(
+    sum(
+        [
+            [
+                pl.col(f"upper_bl_ue/vinf_{i}") < -20,
+                pl.col(f"upper_bl_ue/vinf_{i}") > 20,
+                pl.col(f"lower_bl_ue/vinf_{i}") < -20,
+                pl.col(f"lower_bl_ue/vinf_{i}") > 20,
+            ]
+            for i in range(Data.N)
+        ],
+        start=[],
+    )
+)
+print(
+    f"Nullifying {int(df.select(c).sum().to_numpy()[0, 0])} rows with non-physical edge velocities..."
+)
+df = df.with_columns(
+    [
+        pl.when(c)
+        .then(0)
+        .otherwise(pl.col("analysis_confidence"))
+        .alias("analysis_confidence"),
+    ]
+    + [
+        pl.when(c).then(None).otherwise(pl.col(col)).alias(col)
+        for col in cols_to_nullify
+    ]
+)
+
+c = pl.any_horizontal(
+    pl.col("Top_Xtr") < 0,
+    pl.col("Top_Xtr") > 1,
+    pl.col("Bot_Xtr") < 0,
+    pl.col("Bot_Xtr") > 1,
+)
+print(
+    f"Nullifying {int(df.select(c).sum().to_numpy()[0, 0])} rows with non-physical transition locations..."
+)
+df = df.with_columns(
+    [
+        pl.when(c)
+        .then(0)
+        .otherwise(pl.col("analysis_confidence"))
+        .alias("analysis_confidence"),
+    ]
+    + [
+        pl.when(c).then(None).otherwise(pl.col(col)).alias(col)
+        for col in cols_to_nullify
+    ]
+)
+
+# Convert just the first 18 columns to a numpy array (float)
+params_all = df.select(df.columns[:18]).to_numpy()
 
 # ----------------------------------------------------
 # Extract unique rows
 # ----------------------------------------------------
-# unique_params = np.unique(params_all, axis=0)
+unique_params = np.unique(params_all, axis=0)
 
-unique_params = kulfans_database
+# unique_params = kulfans_database
 
 zscores = (unique_params - unique_params.mean(axis=0)) / unique_params.std(axis=0)
 outlier_mask = np.any(np.abs(zscores) > 3 , axis=1)
@@ -271,7 +276,7 @@ for params in unique_params:
     plt.plot(x, y, color="blue", alpha=0.15, linewidth=1)
 
 plt.gca().set_aspect("equal", adjustable="box")
-plt.title("Kulfan Airfoils used for training data generation")
+plt.title("Kulfan Airfoils in Training Data")
 plt.xlabel("x")
 plt.ylabel("y")
 plt.grid(True, linestyle="--", alpha=0.3)
