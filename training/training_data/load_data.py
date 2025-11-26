37c956001940985100f83dca34943be7d130b758
faiza-alvi~ <bookisharchitect@gmail.com~>
2025-11-26 11:02:17 -0800

    Change file path

--- training/training_data/load_data.py
+++ training/training_data/load_data.py
@@ -133,7 +133,7 @@ cols = Data.get_vector_column_names()
 ### Read the original data, by scraping all .csv files within the data directory
 # data_directory = Path(r"/home/faiza/Documents/NeuralFoil/training/training_data")
 # data_directory = Path(r"/home/faiza/Downloads/training_data/training_data")
-data_directory = Path(r"/home/faiza/Documents/NeuralFoil")
+data_directory = Path(r"/home/faiza/Documents/Gen2TrainingAirfoils")
 
 raw_dfs = {}
 
