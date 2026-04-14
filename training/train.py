bcc55f1de2664e5dfefbfd6eb69b6dccdc97ef23
faiza-alvi~ <bookisharchitect@gmail.com~>
2026-04-14 10:25:37 -0700

    Change file name and use AMP

--- training/train.py
+++ training/train.py
@@ -19,7 +19,7 @@ import gc
 N_inputs = len(df_train_inputs_scaled.columns)
 N_outputs = len(df_train_outputs_scaled.columns)
 
-cache_file = Path(__file__).parent / "nn-avian-gen2-256-baseline.pth"
+cache_file = Path(__file__).parent / "nn-avian-gen2-256-amp.pth"
 n_hidden_layers = 5
 width = 512
 print("Cache file: ", cache_file)
@@ -168,7 +168,7 @@ if __name__ == "__main__":
     )
     
     # Implementation of AMP Flag
-    # scaler = torch.cuda.amp.GradScaler()
+    scaler = torch.cuda.amp.GradScaler()
 
     try:
         checkpoint = torch.load(cache_file)
